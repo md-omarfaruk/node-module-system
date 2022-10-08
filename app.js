267a9d39688e71e282b622d5912e41931bd54e8f
md-omarfaruk <87226097+md-omarfaruk@users.noreply.github.com>
2022-10-08 21:53:48 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const area = require('./area');
+
+console.log(area.circleArea(2));
+
+console.log(area.squareArea(31))
+console.log(area.triangle(3, 3));
---
+++ area.js
@@ -0,0 +1,10 @@
+const circleArea = r => 3.1416 * r * r;
+const squareArea = a => a * a;
+const triangle = (b, h) => .5 * b * h;
+
+
+module.exports. circleArea = circleArea;
+
+module.exports. squareArea = squareArea;
+
+module.exports.triangle = triangle;
